9d1a968f36a984d59aa1b66a216eccbc69f8dd95
Bryan <bryandesigning@gmail.com>
2015-03-05 14:53:54 -0500

    Day 2 Intro - Init Commit
    Created HTML & JS File for Day 2

---
+++ Day 2/js/script.js
@@ -0,0 +1,20 @@
+/*
+ * Bryan Gillespie
+ * 3-5-2015
+ * Section 00
+ * Day 2 Intro
+ */
+
+//alert("Hello World");
+
+/*
+ * Example of a multi lined comment
+ * Anything inside here will not be
+ * processed by the computer.
+ */
+
+//Single line comment - Only this line will be ignored
+
+//Alert Boxes - Popup box that alerts the user with information
+alert("Text to display");
+
