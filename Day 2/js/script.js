809fdf70636ea0ff00555cd946317ced9277b9c8
Bryan <bryandesigning@gmail.com>
2015-03-05 15:42:00 -0500

    Basic Variables
    Updates to Script.js
    
    Reviewing Variables

--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -16,5 +16,38 @@
 //Single line comment - Only this line will be ignored
 
 //Alert Boxes - Popup box that alerts the user with information
-alert("Text to display");
+//alert("Text to display");
 
+//Print to console.log - Shows information to the programmer.
+//Great for checking values and debug
+//Also shows all errors
+
+console.log("This is the console");
+
+//Declare a variable
+//Use the keyword 'var'
+
+var myVariable;
+
+//Define the variable
+
+myVariable = 42;
+
+//Console.log the variable
+
+console.log("myVariable: " + myVariable);
+
+//Declare and define a variable at the same time
+
+var anotherVar = 2;
+
+//Console.log the variable
+
+console.log("anotherVariable: " + anotherVar);
+
+//Add to my variable
+
+anotherVar = anotherVar + 3;
+
+//Print it
+console.log("anotherVariable: " + anotherVar);
