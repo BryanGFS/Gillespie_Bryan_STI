0edaaba77d063d3b16a2c982bb58746ced29d40f
Bryan <bryandesigning@gmail.com>
2015-03-14 16:34:10 -0400

    While loops

--- Day 6/WhileLoops/js/script.js
+++ Day 6/WhileLoops/js/script.js
@@ -4,3 +4,30 @@
  * Section 00
  * Day 6 While Loops
  */
+
+//Basic while loop
+//Start the counter
+
+var counter = 0;
+
+while (counter < 200) {
+
+    console.log("Somethings never end...");
+    console.log("Current Level: " + counter);
+
+    counter++;
+
+}
+
+do {
+    //code to run
+} while (true);
+
+var i = 0;
+do {
+
+    console.log("I: " + i);
+
+    i++;
+
+} while ( i < 10 );
