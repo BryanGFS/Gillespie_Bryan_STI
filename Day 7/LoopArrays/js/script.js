505766ea506b1119fa193afd165a7790f69bd18e
Bryan <bryandesigning@gmail.com>
2015-03-17 14:32:38 -0400

    LoopArrays :S

---
+++ Day 7/LoopArrays/js/script.js
@@ -0,0 +1,12 @@
+/*
+ * Bryan Gillespie
+ * 3-9-2015
+ * Section 00
+ * Day 7 Loop Arrays
+ */
+
+var myArray = ["One","Two","Three","Four","Five"];
+
+for(var i = 0; i < myArray.length; i++) {
+    console.log("Array Value " + myArray[i]);
+}
