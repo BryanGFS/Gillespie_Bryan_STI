144dc99653bdb90eb0a2c0e3697bb778bce2fb39
Bryan <bryandesigning@gmail.com>
2015-03-17 14:57:37 -0400

    yay.... loops :/

--- Day 7/LoopArrays/js/script.js
+++ Day 7/LoopArrays/js/script.js
@@ -10,3 +10,21 @@ var myArray = ["One","Two","Three","Four","Five"];
 for(var i = 0; i < myArray.length; i++) {
     console.log("Array Value " + myArray[i]);
 }
+
+var bills = [50,10,5,20,10];
+var total = 0;
+var average = 0;
+var count = 0;
+
+for(var i=0; i < bills.length; i++) {
+
+    if(bills[i] >= 15) {
+        total += bills[i];
+        count++;
+    }
+
+}
+
+average = total/count;
+
+console.log("Total is " + total + " and the average is " + average);
