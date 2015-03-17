04eb1ce0a207eacf9f66d5779343f74a85444d58
Bryan <bryandesigning@gmail.com>
2015-03-17 15:25:08 -0400

    Functions

---
+++ Day 7/functions/js/script.js
@@ -0,0 +1,25 @@
+/*
+ * Bryan Gillespie
+ * 3-9-2015
+ * Section 00
+ * Day 7 Functions
+ */
+
+function printHello() {
+    console.log("Hello!");
+}
+
+function printMore() {
+    console.log("Print More Text");
+}
+
+function calcArea(x, y) {
+
+    console.log("Area: " + x*y);
+}
+
+printHello();
+
+printMore();
+
+calcArea(5,5);
